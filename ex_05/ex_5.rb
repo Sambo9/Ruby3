df5d12bb1b74bd227147914002d1819cc8e4c056
Sambo <losangeles9@gmail.com>
2016-01-13 17:28:21 +0100

    ex_05

---
+++ ex_05/ex_5.rb
@@ -0,0 +1,23 @@
+#!/usr/bin ruby
+
+
+                        # ================================
+                        # ========= Class Parrot =========
+                        # ================================
+
+class Parrot
+  def method_missing(meth)
+    if "#{meth}".start_with? "say_"
+      word = "#{meth}".gsub("say_","").gsub("_"," ")
+      puts word
+    else
+      super
+    end
+  end
+end
+
+p = Parrot.new
+p.say_
+p.say_hello
+p.say_toto_le_gros_chat
+p.xdrcfgvhbjn
